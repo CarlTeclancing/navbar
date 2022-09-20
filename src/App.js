d22a69b3b211bb159cea1054f471a916b86120e4
YuvCarl <carldrake969@gmail.com>
2022-09-20 03:15:11 +0100

    updated the bio.jsx file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header'
+import Bio from './components/Bio'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <Header title = "carl in" name = "carl in"/>
+        <Bio/>
     </div>
   );
 }
--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -2,7 +2,7 @@ function Bio(){
     return(
         <>
             <div>
-                
+                <h1>this is the boi section</h1>
             </div>
         
         </>
